507340c022be187563089d2b39e5cba1ddafe856
AvertinDeEmperana <avertin.sayen@gmail.com>
2022-02-24 18:15:19 +0100

    test release

--- lib/src/data/api/base_api.dart
+++ lib/src/data/api/base_api.dart
@@ -1,10 +1,9 @@
-import '../../env_config.dart';
-
-final envConf = EnvironmentConf();
+import '../../../env.dart';
 
 abstract class BaseApiService {
   final String baseUrl = "https://newsapi.org/v2/";
-  final String apiKey = envConf.theApiKey;
+  //final apiKey = EnvConfig.api_key;
+  final apiKey = "42b8be96647240dbbde0d6e6fa7016d0";
 
   Future<dynamic> getResponse(String query);
 }
